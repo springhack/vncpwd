447ae97017dcc6b893bbd93bf551c677e8e9766f
springhack <springhack@live.cn>
2021-03-04 13:24:45 +0800

    Add setter

--- vncpwd.c
+++ vncpwd.c
@@ -1,23 +1,6 @@
-/*
-   VNC Password Decrypter
-   Copyright (c) 2018 Jeroen Nijhof <jeroen@jeroennijhof.nl>
-
-   This program is free software: you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation, either version 3 of the License, or
-   (at your option) any later version.
-
-   This program is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
-
-   You should have received a copy of the GNU General Public License
-   along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/types.h>
 #include <unistd.h>
 #include "d3des.h"
@@ -34,25 +17,53 @@ void decryptPw( unsigned char *pPW ) {
     fprintf(stdout, "Password: %s\n", clrtxt);
 }
 
+void encryptPw(unsigned char * pw, FILE* fp) {
+  unsigned char bytes[100] = {0};
+
+  deskey(obfKey, EN0);
+  des(pw, bytes);
+
+  fwrite(bytes, sizeof(char) * 8, 1, fp);
+
+  printf("Write binary: ");
+  for (int i = 0; i < 8; ++i)
+    printf("0x%x ", bytes[i]);
+  printf("\n");
+}
+
 int main(int argc, char *argv[]) {
     FILE *fp;
     unsigned char *pwd;
 
-    if (argc < 2) {
-        fprintf(stdout, "Usage: vncpwd <password file>\n");
+    if (argc < 3) {
+        fprintf(stdout, "Usage: vncpwd <password file> <mode> [password]\n");
         return 1;
     }
 
-    if ((fp = fopen(argv[1], "r")) == NULL) {
-        fprintf(stderr, "Error: can not open password file: %s\n", argv[1]);
-        return 1;
+    if (strcmp(argv[2], "get") == 0) {
+      if ((fp = fopen(argv[1], "r")) == NULL) {
+          fprintf(stderr, "Error: can not open password file: %s\n", argv[1]);
+          return 1;
+      }
+      pwd = malloc(1024);
+      fread(pwd, 1024, 1, fp);
+      decryptPw(pwd);
+      fclose(fp);
+      free(pwd);
     }
-    pwd = malloc(1024);
-    fread(pwd, 1024, 1, fp);
-    fclose(fp);
 
-    decryptPw(pwd);
-
-    free(pwd);
+    if (strcmp(argv[2], "set") == 0) {
+      if (argc < 4) {
+        fprintf(stdout, "Usage: vncpwd <password file> <mode> [password]\n");
+        return 1;
+      }
+      if ((fp = fopen(argv[1], "w")) == NULL) {
+          fprintf(stderr, "Error: can not open password file: %s\n", argv[1]);
+          return 1;
+      }
+      encryptPw((unsigned char *)argv[3], fp);
+      fclose(fp);
+    }
+ 
     return 0;
 }
